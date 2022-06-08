d8077feede62a2c679f912118863709d9819034b
Igor Shatava <cianoid@yandex.ru>
2022-06-08 23:18:59 +0900

    add redoc

--- quadratic/quadratic/settings.py
+++ quadratic/quadratic/settings.py
@@ -40,10 +40,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'quadratic.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,6 +113,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
