17cccbd05fcd4bad420b3dc437f2b213ec16d012
Igor Shatava <cianoid@yandex.ru>
2022-06-10 07:12:05 +0900

    fix infra

--- quadratic/quadratic/urls.py
+++ quadratic/quadratic/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('secure/', admin.site.urls),
@@ -9,3 +10,5 @@ urlpatterns = [
         'redoc/', TemplateView.as_view(template_name='redoc.html'),
         name='redoc'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
